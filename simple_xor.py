3e327212e97b412411b06ac5e5b6baa654c430b8
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-07-03 15:47:02 +1000

    updated number of hidden neurons and decreased number of iterations

--- simple_xor.py
+++ simple_xor.py
@@ -7,12 +7,12 @@ x = tf.placeholder(tf.float32, shape=[None, 2], name='input')
 y_ = tf.placeholder(tf.float32, shape=[None, 2], name='correct')
 
 with tf.name_scope("layer1"):
-    W1 = tf.Variable(tf.random_normal(shape=[2, 3]), name="weights")
-    b1 = tf.Variable(tf.random_normal(shape=[3]), name="bias")
+    W1 = tf.Variable(tf.random_normal(shape=[2, 10]), name="weights")
+    b1 = tf.Variable(tf.random_normal(shape=[10]), name="bias")
     h1 = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
 
 with tf.name_scope("layer2"):
-    W2 = tf.Variable(tf.random_normal(shape=[3, 2]), name="weights")
+    W2 = tf.Variable(tf.random_normal(shape=[10, 2]), name="weights")
     b2 = tf.Variable(tf.random_normal(shape=[2]), name="bias")
     y = tf.matmul(h1, W2) + b2
 
@@ -20,7 +20,7 @@ with tf.name_scope("cost"):
     loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 with tf.name_scope("train"):
-    train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss_op)
+    train_op = tf.train.GradientDescentOptimizer(0.5).minimize(loss_op)
 
 prediction = tf.argmax(tf.nn.softmax(y), 1)
 true = tf.argmax(y_, 1)
@@ -32,7 +32,7 @@ writer = tf.summary.FileWriter("xor_log", graph=tf.get_default_graph())
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for i in range(10000):
+    for i in range(500):
         loss, _ = sess.run([loss_op, train_op], feed_dict={x: samples_x, y_: samples_y})
         print(loss)
 
