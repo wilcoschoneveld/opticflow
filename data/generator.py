e4932e7fd6ae379982a962a29bd5487f102d784f
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-07-26 20:54:16 +1000

    added subpixel flow and normalization in data generator

--- data/generator.py
+++ data/generator.py
@@ -6,12 +6,15 @@ import random
 
 class DataGenerator(object):
 
-    def __init__(self, pattern='images/*', image_size=64, max_flow=10, min_scale=1, max_scale=5, noise_level=5, interp='bicubic'):
+    def __init__(self, pattern='images/*', image_size=64, max_flow=10, min_scale=1, max_scale=5, noise_level=5,
+                 sub_pixel_flow=True, normalize=False, interp='bicubic'):
         self.image_size = image_size
         self.max_flow = max_flow
         self.min_scale = min_scale
         self.max_scale = max_scale
         self.noise_level = noise_level
+        self.sub_pixel_flow = sub_pixel_flow
+        self.normalize = normalize
         self.interp = interp
 
         self.images = [scipy.misc.imread(f, flatten=True) for f in glob.glob(pattern)]
@@ -23,7 +26,11 @@ class DataGenerator(object):
         image = random.choice(self.images)
 
         scale = np.random.randint(self.min_scale, self.max_scale + 1)
-        flow = np.random.randint(-self.max_flow*scale, self.max_flow*scale, size=2)
+
+        if self.sub_pixel_flow:
+            flow = np.random.randint(-self.max_flow*scale, self.max_flow*scale, size=2)
+        else:
+            flow = np.random.randint(-self.max_flow, self.max_flow, size=2) * scale
 
         y0 = np.random.randint(self.max_flow*scale, image.shape[0] - self.image_size*scale - self.max_flow*scale)
         x0 = np.random.randint(self.max_flow*scale, image.shape[1] - self.image_size*scale - self.max_flow*scale)
@@ -37,14 +44,21 @@ class DataGenerator(object):
         if scale > 1:
             image0 = scipy.misc.imresize(image0, (self.image_size, self.image_size), self.interp)
             image1 = scipy.misc.imresize(image1, (self.image_size, self.image_size), self.interp)
+            flow = flow / scale
 
         if self.noise_level > 0:
             noise = np.random.normal(0, self.noise_level, (2, self.image_size, self.image_size))
 
-            image0 = np.clip(image0 + noise[0], 0, 255).round().astype(np.uint8)
-            image1 = np.clip(image1 + noise[1], 0, 255).round().astype(np.uint8)
+            image0 = np.clip(image0 + noise[0], 0, 255)
+            image1 = np.clip(image1 + noise[1], 0, 255)
 
-        return image0, image1, flow / scale
+        if self.normalize:
+            norm0 = (image0 - np.mean(image0)) / np.var(image0)
+            norm1 = (image1 - np.mean(image1)) / np.var(image1)
+
+            return norm0, norm1, flow
+
+        return image0.astype(np.uint8), image1.astype(np.uint8), flow
 
     def generate_batch(self, batch_size):
         inputs = np.empty((batch_size, self.image_size, self.image_size, 2))
